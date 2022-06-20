2d12e4903abfd276c1452fef5b967828f4d6a43a
unvise <freezey59@gmail.com>
2022-06-21 00:21:01 +0300

    Add a starter spring boot project

---
+++ src/main/java/com/unvise/bankingsystemapp/BankingSystemAppApplication.java
@@ -0,0 +1,13 @@
+package com.unvise.bankingsystemapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankingSystemAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankingSystemAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unvise/bankingsystemapp/BankingSystemAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unvise.bankingsystemapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingSystemAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
