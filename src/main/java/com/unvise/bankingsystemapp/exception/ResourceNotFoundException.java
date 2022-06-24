cd8771dbfbea88639040fc312a0124d6647d68d4
unvise <freezey59@gmail.com>
2022-06-24 10:46:01 +0300

    Add exception for resource not found(404).

---
+++ src/main/java/com/unvise/bankingsystemapp/exception/ResourceNotFoundException.java
@@ -0,0 +1,19 @@
+package com.unvise.bankingsystemapp.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@Getter
+@AllArgsConstructor
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException {
+
+    private static final long serialVersionUID = 1L;
+
+    private String resourceName;
+    private String fieldName;
+    private Object fieldValue;
+
+}
