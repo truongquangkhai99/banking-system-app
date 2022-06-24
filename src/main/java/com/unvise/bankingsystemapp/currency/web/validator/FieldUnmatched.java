29d82e95a0af2e385d7601e7c0099650d7e3b2ad
unvise <freezey59@gmail.com>
2022-06-24 16:02:09 +0300

    Add a field unmatched validator to avoid having the same values for currency fields.

---
+++ src/main/java/com/unvise/bankingsystemapp/currency/web/validator/FieldUnmatched.java
@@ -0,0 +1,26 @@
+package com.unvise.bankingsystemapp.currency.web.validator;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+import java.lang.annotation.*;
+
+@Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = FieldUnmatchedValidator.class)
+@Documented
+public @interface FieldUnmatched
+{
+    String message() default "The fields mustn't match";
+    Class<?>[] groups() default {};
+    Class<? extends Payload>[] payload() default {};
+    String first();
+    String second();
+
+    @Target({ElementType.TYPE, ElementType.ANNOTATION_TYPE})
+    @Retention(RetentionPolicy.RUNTIME)
+    @Documented
+    @interface List
+    {
+        FieldUnmatched[] value();
+    }
+}
