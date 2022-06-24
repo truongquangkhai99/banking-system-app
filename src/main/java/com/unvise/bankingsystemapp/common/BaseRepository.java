aaa4b4fad10f87d29fd379b810babf3e7bd8f142
unvise <freezey59@gmail.com>
2022-06-24 13:38:57 +0300

    Refactor. Add a new generic param ID

--- src/main/java/com/unvise/bankingsystemapp/common/BaseRepository.java
+++ src/main/java/com/unvise/bankingsystemapp/common/BaseRepository.java
@@ -3,6 +3,8 @@ package com.unvise.bankingsystemapp.common;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
+import java.io.Serializable;
+
 @NoRepositoryBean
-public interface BaseRepository<T> extends JpaRepository<T, Long> {
+public interface BaseRepository<T, ID extends Serializable> extends JpaRepository<T, ID> {
 }
