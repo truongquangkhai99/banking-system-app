7eceb4d165e788e5f61cb3fd6ef66eda72f11ec2
unvise <freezey59@gmail.com>
2022-06-25 22:09:16 +0300

    Add a common fields with audit.

--- src/main/java/com/unvise/bankingsystemapp/credit/Credit.java
+++ src/main/java/com/unvise/bankingsystemapp/credit/Credit.java
@@ -1,24 +1,40 @@
 package com.unvise.bankingsystemapp.credit;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import com.unvise.bankingsystemapp.audit.DateAudit;
+import lombok.*;
 
 import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "credit")
 @Builder
 @Data
+@EqualsAndHashCode(callSuper = true)
 @NoArgsConstructor
 @AllArgsConstructor
-public class Credit {
+public class Credit extends DateAudit {
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "credit_seq_gen")
     @SequenceGenerator(name = "credit_seq_gen", sequenceName = "credit_seq_id", allocationSize = 1)
     @Column(name = "id", nullable = false)
     private Long id;
+
+    @Column(name = "total", nullable = false, precision = 18, scale = 4)
+    private BigDecimal total;
+
+    @Column(name = "current", nullable = false, precision = 18, scale = 4)
+    private BigDecimal current;
+
+    @Column(name = "remain", nullable = false, precision = 18, scale = 4)
+    private BigDecimal remain;
+
+    @Column(name = "date_between_payments", nullable = false)
+    private LocalDate dateBetweenPayments;
+
+    @Column(name = "is_closed")
+    private Boolean isClosed;
 
 }
