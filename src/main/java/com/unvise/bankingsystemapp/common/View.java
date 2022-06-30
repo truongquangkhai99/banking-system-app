50463a9a39e511f3821c2249fa18278c47d65ce3
unvise <freezey59@gmail.com>
2022-06-30 23:24:52 +0300

    Add a global view

---
+++ src/main/java/com/unvise/bankingsystemapp/common/View.java
@@ -0,0 +1,23 @@
+package com.unvise.bankingsystemapp.common;
+
+public final class View {
+
+
+    /**
+     * Used when saving new data
+     */
+    public interface New { }
+
+
+    /**
+     * Used when updating existing data
+     */
+    public interface Update { }
+
+    /**
+     * Used to display details.
+     * Ex. id, name, phone and etc
+     */
+    public interface Details { }
+
+}
