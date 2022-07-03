fa93ea4ff425a6cfd10091538dea2aa8fe4d549b
unvise <freezey59@gmail.com>
2022-07-03 18:37:27 +0300

    Refactor

--- src/main/java/com/unvise/bankingsystemapp/exception/ResourceNotFoundException.java
+++ src/main/java/com/unvise/bankingsystemapp/exception/ResourceNotFoundException.java
@@ -5,7 +5,6 @@ import lombok.Getter;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-import java.util.HashMap;
 import java.util.Map;
 
 @Getter
