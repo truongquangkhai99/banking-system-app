d081903a7fd8067fb16ad0b110076ea39c993435
unvise <freezey59@gmail.com>
2022-07-03 18:47:44 +0300

    Add a new validation annotations.

--- src/main/java/com/unvise/bankingsystemapp/account/web/dto/AccountSecurityDetailsDto.java
+++ src/main/java/com/unvise/bankingsystemapp/account/web/dto/AccountSecurityDetailsDto.java
@@ -1,10 +1,12 @@
 package com.unvise.bankingsystemapp.account.web.dto;
 
+import com.unvise.bankingsystemapp.common.View;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Null;
 
 @Builder
@@ -13,9 +15,10 @@ import javax.validation.constraints.Null;
 @AllArgsConstructor
 public class AccountSecurityDetailsDto {
 
-    @Null
+    @Null(groups = {View.New.class, View.Update.class})
     private Long id;
 
+    @NotNull(groups = {View.New.class, View.Update.class})
     private String passwordHash;
 
 }
