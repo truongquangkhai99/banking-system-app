0ea726c1c93a2ab3f8f5acf4cc1e0750470abc3a
unvise <freezey59@gmail.com>
2022-07-12 02:19:02 +0300

    Update security config

--- src/main/java/com/unvise/bankingsystemapp/domain/auth/SecurityConfig.java
+++ src/main/java/com/unvise/bankingsystemapp/domain/auth/SecurityConfig.java
@@ -1,8 +1,13 @@
 package com.unvise.bankingsystemapp.domain.auth;
 
+import com.unvise.bankingsystemapp.domain.auth.jwt.JwtTokenFilter;
 import com.unvise.bankingsystemapp.domain.person.role.RoleType;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,42 +15,61 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@RequiredArgsConstructor
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 @Configuration
-@RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final UserDetailsService userDetailsService;
+    private final JwtTokenFilter jwtTokenFilter;
     private final BCryptPasswordEncoder encoder;
 
+    @Bean
     @Override
-    public void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService).passwordEncoder(encoder);
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    @Bean
+    public DaoAuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService);
+        authProvider.setPasswordEncoder(encoder);
+
+        return authProvider;
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(authenticationProvider());
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http
-                .csrf().disable()
+        http.
+                csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/admin/**").hasAuthority(RoleType.ADMIN.getRoleTypeAsString())
                 .antMatchers("/swagger-ui/**").hasAuthority(RoleType.ADMIN.getRoleTypeAsString())
                 .antMatchers("/api/**").hasAuthority(RoleType.ADMIN.getRoleTypeAsString())
-                .antMatchers("/").permitAll()
+                .antMatchers("/", "/signin", "/signup").permitAll()
+                .antMatchers(HttpMethod.POST, "/auth/jwt/**").permitAll()
                 .anyRequest().authenticated()
                 .and()
                 .formLogin()
+                .loginPage("/signin")
                 .usernameParameter("email")
-                .passwordParameter("password")
-                .defaultSuccessUrl("/", true)
+                .loginProcessingUrl("/perform-login")
                 .permitAll()
+                .and()
+                .logout().permitAll()
                 .and()
                 .httpBasic()
                 .and()
-                .logout()
-                .permitAll();
+                .addFilterAfter(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
     }
-    
+
 }
