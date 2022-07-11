7b3ef1eb7b8772c461fadf2fdd75b48d10395d51
unvise <freezey59@gmail.com>
2022-07-12 01:00:09 +0300

    Remove the pattern for the date in a separate field as a constant

--- src/main/java/com/unvise/bankingsystemapp/domain/transaction/web/dto/TransactionDto.java
+++ src/main/java/com/unvise/bankingsystemapp/domain/transaction/web/dto/TransactionDto.java
@@ -19,11 +19,13 @@ import javax.validation.constraints.Null;
 @ValidateTransaction(groups = {View.New.class})
 public class TransactionDto {
 
+    public static final String DATE_PATTERN = "yyyy-MM-dd";
+
     @Null(message = "id must be null", groups = {View.New.class})
     private Long id;
 
-    @NotNull(message = "date must be not null and have format 'yyyy-MM-dd'", groups = {View.New.class})
-    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    @NotNull(message = "date must be not null and have format " + DATE_PATTERN, groups = {View.New.class})
+    @DateTimeFormat(pattern = DATE_PATTERN)
     private String date;
 
     @NotNull(message = "transaction_details must be not null with json object with fields inside", groups = {View.New.class})
