f834a1df78d62b41562dffeed47ad83066082dbe
unvise <freezey59@gmail.com>
2022-07-12 02:37:22 +0300

    Refactor. Move HomeController to web.mvc package.

--- src/main/java/com/unvise/bankingsystemapp/domain/common/web/mvc/HomeController.java
+++ src/main/java/com/unvise/bankingsystemapp/domain/common/web/mvc/HomeController.java
@@ -1,4 +1,4 @@
-package com.unvise.bankingsystemapp.domain.common;
+package com.unvise.bankingsystemapp.domain.common.web.mvc;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
