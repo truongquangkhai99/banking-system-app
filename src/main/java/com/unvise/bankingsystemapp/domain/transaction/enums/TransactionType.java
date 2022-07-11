5b9fa216ae7bcdf0acf0d982510aa30b771184dd
unvise <freezey59@gmail.com>
2022-07-12 01:02:30 +0300

    Add a TOP_UP_ACCOUNT_BALANCE and WITHDRAW_DEPOSIT enums.

--- src/main/java/com/unvise/bankingsystemapp/domain/transaction/enums/TransactionType.java
+++ src/main/java/com/unvise/bankingsystemapp/domain/transaction/enums/TransactionType.java
@@ -6,7 +6,11 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum TransactionType {
 
-    DEPOSIT("DEPOSIT"), TRANSFER("TRANSFER"), CREDIT("CREDIT");
+    DEPOSIT("DEPOSIT"),
+    TRANSFER("TRANSFER"),
+    CREDIT("CREDIT"),
+    TOP_UP_ACCOUNT_BALANCE("TOP_UP_ACCOUNT_BALANCE"),
+    WITHDRAW_DEPOSIT("WITHDRAW_DEPOSIT");
 
     @Getter
     private final String transactionTypeAsString;
