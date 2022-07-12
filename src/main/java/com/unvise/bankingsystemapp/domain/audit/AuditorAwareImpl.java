c6b085179c7fdd17c468662023f8203cae5e204a
unvise <freezey59@gmail.com>
2022-07-12 12:18:18 +0300

    Remove comment

--- src/main/java/com/unvise/bankingsystemapp/domain/audit/AuditorAwareImpl.java
+++ src/main/java/com/unvise/bankingsystemapp/domain/audit/AuditorAwareImpl.java
@@ -5,8 +5,6 @@ import org.springframework.data.domain.AuditorAware;
 import java.util.Optional;
 
 public class AuditorAwareImpl implements AuditorAware<String> {
-
-    // todo: refactor this after add Spring Security
     @Override
     public Optional<String> getCurrentAuditor() {
         return Optional.of("unvise");
